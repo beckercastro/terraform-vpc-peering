161c1753f176c6d02fc5735b1a28438d0400ee74
beckercastro <33099366+beckercastro@users.noreply.github.com>
2019-06-07 17:09:01 -0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,21 +1,30 @@
 variable "owner_profile" {
     description = "AWS Profile"
-    default     = "Development"
-}
-variable "region" {
-    description = "AWS region"
-    default     = "us-east-1"
+    default     = "development"
 }
 
 variable "accepter_profile" {
     description = "AWS Profile"
-    default     = "default"
+    default     = "production"
 }
 
 variable "owner_vpc_id" {
   description = "Owner VPC ID"
+  default     = "vpc-5af9c221"
 }
 
 variable "accepter_vpc_id" {
     description = "Accepter VPC ID"
+    default     = "vpc-0a66767beb5bda9f4"
+}
+
+
+variable "region_owner" {
+    description = "AWS region"
+    default     = "us-east-1"
+}
+
+variable "region_accepter" {
+    description = "AWS region"
+    default     = "sa-east-1"
 }
